d40fa60efa694080e7363f68bc5a6fe8c51f0c7e
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-06-13 00:07:16 +0600

    Basic Layout of the system and necessary dependency

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Container from "../../Container";
+
+const Home = () => {
+  return (
+    <Container>
+      <div>
+        <h1 className="bg-red-500">banner</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,13 @@
+import Container from "./../Container";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <div>
+        <h1>navbar</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Navbar;
